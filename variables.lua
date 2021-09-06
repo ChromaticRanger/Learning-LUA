eb6217921cdd783a6242c25c3c78b40837aef760
Martin Stickley <mds1966@gmail.com>
2021-09-06 21:48:24 +0100

    LUA variables

---
+++ variables.lua
@@ -0,0 +1,22 @@
+--[[ Declaring and printing variables --]]
+
+-- Variable Definition
+local a, b
+
+-- Initialisation
+a = 10
+b = 30
+
+print("value of a:", a)
+print("value of b:", b)
+
+-- Swapping of variables
+b, a = a, b
+
+print("value of a:", a)
+print("value of b:", b)
+
+_F = 70.0/3.0
+
+print("value of f:", _F)
+
